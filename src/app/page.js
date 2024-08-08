184739ef07e44a16d8dd1a5ef71c29198ccfc0fd
qeti buzaladze <buzaladzeqeti16@gmail.com>
2024-08-08 15:22:06 +0400

    qeti

---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textWrapper}>
+        <h1 className={styles.title}>Welcome!:)</h1>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt="brands"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+			<div className={styles.imgContainer}>
+			<Image src="/welcome.png" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <h4>BUZALADZE</h4>
+      <p className={styles.text}>
+        CodeIT © All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
